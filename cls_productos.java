d2e804b176eab5d83543292a4f6521295d0b9314
Yeison Cordoba <corba.ye99@gmail.com>
2024-02-21 17:07:24 -0500

    Cambios al constructor y eliminando setters

--- cls_productos.java
+++ cls_productos.java
@@ -6,27 +6,20 @@ public class cls_productos {
     private float precio_venta_flt;
     private float ganancia_flt;
 
-    public cls_productos(int codigo_int, String nombre_str, int existencias_int, float precio_venta_flt,
-            float ganancia_flt) {
+    public cls_productos(int codigo_int, String nombre_str, float ganancia_flt) {
         this.codigo_int = codigo_int;
         this.nombre_str = nombre_str;
-        this.existencias_int = existencias_int;
-        this.precio_venta_flt = precio_venta_flt;
+        this.existencias_int = 0;
+        this.precio_venta_flt = 0;
         this.ganancia_flt = ganancia_flt;
     }
 
     public int getCodigo_int() {
         return codigo_int;
     }
-    public void setCodigo_int(int codigo_int) {
-        this.codigo_int = codigo_int;
-    }
 
     public String getNombre_str() {
         return nombre_str;
-    }
-    public void setNombre_str(String nombre_str) {
-        this.nombre_str = nombre_str;
     }
 
     public int getExistencias_int() {
