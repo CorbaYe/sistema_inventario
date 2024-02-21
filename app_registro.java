b17a3c738e47c12f00420342109fe4a9d291f26a
Yeison Cordoba <corba.ye99@gmail.com>
2024-02-21 17:43:15 -0500

    Terminando funcion actualizar y optimizando funcion consultar

--- app_registro.java
+++ app_registro.java
@@ -14,9 +14,23 @@ public class app_registro {
     private static void fnt_actualizar(int codigo_int){
         fnt_consultar(codigo_int);
         if (sw) {
-            JOptionPane.showMessageDialog(null, "Nombre: " + productos.get(posicion).getNombre_str() + "\n" + "Existencias: " + productos.get(posicion).getExistencias_int() + "\n" + "Precio de venta: " + productos.get(posicion).getPrecio_venta_flt() + "\n" + "Ganancia: " + productos.get(posicion).getGanancia_flt(), "CONSULTAR",JOptionPane.INFORMATION_MESSAGE);
+            boolean menu_actualizar = true;
+            do {
+                String menu = JOptionPane.showInputDialog(null,"MENU ACTUALIZAR\n\n1. Actualizar nombre\n2. Actualizar ganancia\n3. Cancelar");
+                if (menu.equals("1")) {
+                    String nombre_str = JOptionPane.showInputDialog(null, "Nombre");
+                    productos.get(posicion).setNombre_str(nombre_str);
+                }
+                if (menu.equals("2")) {
+                    Float ganancia_flt = Float.valueOf(JOptionPane.showInputDialog(null, "Ganancia")); 
+                    productos.get(posicion).setGanancia_flt(ganancia_flt);
+                }
+                if (menu.equals("3") || !menu.equals("1") || !menu.equals("2") || !menu.equals("3") ) {
+                    menu_actualizar = false;
+                }
+            } while (menu_actualizar);
         }else{
-            JOptionPane.showMessageDialog(null,"Producto no encontrado.","CONSULTAR",JOptionPane.WARNING_MESSAGE);
+            JOptionPane.showMessageDialog(null,"Producto no encontrado.","ACTUALIZAR",JOptionPane.WARNING_MESSAGE);
         }
     }
 
@@ -28,11 +42,6 @@ public class app_registro {
                 posicion = i;
                 sw = true;
             }
-        }
-        if (sw) {
-            JOptionPane.showMessageDialog(null, "Nombre: " + productos.get(posicion).getNombre_str() + "\n" + "Existencias: " + productos.get(posicion).getExistencias_int() + "\n" + "Precio de venta: " + productos.get(posicion).getPrecio_venta_flt() + "\n" + "Ganancia: " + productos.get(posicion).getGanancia_flt(), "CONSULTAR",JOptionPane.INFORMATION_MESSAGE);
-        }else{
-            JOptionPane.showMessageDialog(null,"Producto no encontrado.","CONSULTAR",JOptionPane.WARNING_MESSAGE);
         }
     }
 
@@ -58,9 +67,15 @@ public class app_registro {
             if (menu.equals("2")) {
                 int codigo_int = Integer.valueOf(JOptionPane.showInputDialog(null, "Ingrese el código del producto")); 
                 fnt_consultar(codigo_int);
+                if (sw) {
+                    JOptionPane.showMessageDialog(null, "Nombre: " + productos.get(posicion).getNombre_str() + "\n" + "Existencias: " + productos.get(posicion).getExistencias_int() + "\n" + "Precio de venta: " + productos.get(posicion).getPrecio_venta_flt() + "\n" + "Ganancia: " + productos.get(posicion).getGanancia_flt(), "CONSULTAR",JOptionPane.INFORMATION_MESSAGE);
+                }else{
+                    JOptionPane.showMessageDialog(null,"Producto no encontrado.","CONSULTAR",JOptionPane.WARNING_MESSAGE);
+                }
             }
             if (menu.equals("3")) {
-                m=false;
+                int codigo_int = Integer.valueOf(JOptionPane.showInputDialog(null, "Ingrese el código del producto a actualizar")); 
+                fnt_actualizar(codigo_int);
             }
             if (menu.equals("4")) {
                 m=false;
--- cls_productos.java
+++ cls_productos.java
@@ -14,33 +14,29 @@ public class cls_productos {
         this.ganancia_flt = ganancia_flt;
     }
 
-    public int getCodigo_int() {
-        return codigo_int;
-    }
-
-    public String getNombre_str() {
-        return nombre_str;
-    }
-
     public int getExistencias_int() {
         return existencias_int;
-    }
-    public void setExistencias_int(int existencias_int) {
-        this.existencias_int = existencias_int;
     }
 
     public float getPrecio_venta_flt() {
         return precio_venta_flt;
     }
-    public void setPrecio_venta_flt(float precio_venta_flt) {
-        this.precio_venta_flt = precio_venta_flt;
+    public int getCodigo_int() {
+        return codigo_int;
     }
-    
+    //
     public float getGanancia_flt() {
         return ganancia_flt;
     }
     public void setGanancia_flt(float ganancia_flt) {
         this.ganancia_flt = ganancia_flt;
+    }
+    //
+    public String getNombre_str() {
+        return nombre_str;
+    }
+    public void setNombre_str(String nombre_str) {
+        this.nombre_str = nombre_str;
     }
     
 }
